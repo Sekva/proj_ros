cd7db862ac7b427d62443ad1629aa2b91f641ad6
Sekva <matheusmachado708@gmail.com>
2019-05-06 16:36:41 -0300

    Workspace com pacote vazio

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/ros/workspace_so/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/ros/workspace_so/devel;/opt/ros/kinetic".split(';') if "/home/ros/workspace_so/devel;/opt/ros/kinetic" != "" else []
---
+++ build/proj/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros/workspace_so/src/proj
+# Build directory: /home/ros/workspace_so/build/proj
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
