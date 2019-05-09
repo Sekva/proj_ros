92a7125b4012f893671be09e533104d45e8d8997
Sekva <matheusmachado708@gmail.com>
2019-05-08 21:14:49 -0300

    arduino lendo e publicano

--- src/proj/scripts/talker.py
+++ src/proj/scripts/talker.py
@@ -4,7 +4,7 @@ import cv2
 from geometry_msgs.msg import Point32
 
 def talker():
-	pub = rospy.Publisher('chatter', Point32, queue_size=10)
+	pub = rospy.Publisher('arduino', Point32, queue_size=10)
 	rospy.init_node('tracker', anonymous=True)
 	rate = rospy.Rate(10) # 10hz
 	while not rospy.is_shutdown():
--- src/proj/src/arduino_no/arduino_no.ino
+++ src/proj/src/arduino_no/arduino_no.ino
@@ -1,9 +1,15 @@
 
 #include <ros.h>
 #include <geometry_msgs/Point32.h>
+#include <std_msgs/Float32.h>
+#include <math.h>
+
 
 ros::NodeHandle  nh;
 
+
+std_msgs::Float32 norma;
+ros::Publisher publicador("arduino_norma", &norma);
 
 float x, y;
 
@@ -18,14 +24,14 @@ void setup()
 {
   pinMode(13, OUTPUT);
   nh.initNode();
+  nh.advertise(publicador);
   nh.subscribe(sub);
 }
 
 void loop()
 {  
-  if( ((int) x) % 5 == 2) {
-    digitalWrite(13, HIGH-digitalRead(13));
-  }
+  norma.data = sqrt(x*x + y*y);
+  publicador.publish(&norma);
   nh.spinOnce();
   delay(1);
 }
