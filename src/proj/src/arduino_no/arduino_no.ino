f879d8a268f5854f1379c793fd3988b19adb53e0
Sekva <matheusmachado708@gmail.com>
2019-06-27 20:36:04 -0300

    versão da norma

--- src/proj/src/arduino_no/arduino_no.ino
+++ src/proj/src/arduino_no/arduino_no.ino
@@ -1,18 +1,20 @@
-
 #include <ros.h>
 #include <geometry_msgs/Point32.h>
 #include <std_msgs/Float32.h>
 #include <math.h>
 
+
 ros::NodeHandle  nh;
+
+
 std_msgs::Float32 norma;
 ros::Publisher publicador("arduino_norma", &norma);
+
 float x, y;
 
 void messageCb( const geometry_msgs::Point32& ponto) {
   x = ponto.x;
   y = ponto.y;
- 
 }
 
 ros::Subscriber<geometry_msgs::Point32> sub("arduino", &messageCb );
@@ -25,15 +27,10 @@ void setup()
   nh.subscribe(sub);
 }
 
-void loop() {
+void loop()
+{  
   norma.data = sqrt(x*x + y*y);
   publicador.publish(&norma);
   nh.spinOnce();
-  
-  if(norma.data > 400) {
-   digitalWrite(13, HIGH-digitalRead(13));   // blink the led 
-  } else {
-    digitalWrite(13, LOW);   
-  }
   delay(1);
 }
